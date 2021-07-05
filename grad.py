fbbe22bae0cc4c76644da2e97fb76be7902d77e9
Aryan Raj <rajaaryan711@gmail.com>
2021-07-06 00:02:05 +0530

    Update grad.py

--- grad.py
+++ grad.py
@@ -13,9 +13,40 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 #from sklearn.feature_extraction.text import CountVectorizer
 
+import pandas as pd
+messages = pd.read_csv('spam1.csv')
 
-model = pickle.load(open('spam_classifier.pkl', 'rb'))
-cv = pickle.load(open('count_vect', 'rb'))
+ps = PorterStemmer()
+corpus = []
+
+for i in range(len(messages)):
+    review = re.sub('[^a-zA-Z]', ' ', messages['message'][i])
+    review = review.lower()
+    review = review.split()
+    
+    review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+    review = ' '.join(review)
+    corpus.append(review)
+    
+#Creating BOW
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=2500)
+X = cv.fit_transform(corpus).toarray()
+
+y = pd.get_dummies(messages['label'])
+y = y.iloc[:,1].values
+#y = messages.label
+
+#Train Test Split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=None)
+
+#Training model using NaiveBayes
+from sklearn.naive_bayes import MultinomialNB
+spam_model = MultinomialNB()
+spam_model.fit(X_train, y_train)
+
+y_pred = spam_model.predict(X_test)
 
 
 def spam_classifier(message):
@@ -30,7 +61,7 @@ def spam_classifier(message):
     
     X = cv.transform(corpus).toarray()
     
-    prediction = model.predict(X)
+    prediction = spam_model.predict(X)
     if prediction==1:
         return 'SPAM!'
     else:
